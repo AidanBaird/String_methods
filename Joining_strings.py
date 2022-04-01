698b8fda3da2e9c5d8732c5146df7ba5ceb9728d
Aidan Baird <102356055+AidanBaird@users.noreply.github.com>
2022-04-01 18:49:19 +0100

    Add files via upload

---
+++ Joining_strings.py
@@ -0,0 +1,9 @@
+reapers_line_one_words = ["Black", "reapers", "with", "the", "sound", "of", "steel", "on", "stones"]
+
+# Create a new variable and use the join command to turn a list into a single string
+
+reapers_line_one = (" ".join(reapers_line_one_words))
+
+print(" ".join(reapers_line_one_words))
+print("")
+print(reapers_line_one)
