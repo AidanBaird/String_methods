00d574a8c5d4ed81f773253fb88ea5f36953fb1b
Aidan Baird <102356055+AidanBaird@users.noreply.github.com>
2022-04-03 11:24:11 +0100

    Add files via upload

--- String_methods_review.py
+++ String_methods_review.py
@@ -12,9 +12,33 @@ highlighted_poems_stripped = []
 for element in highlighted_poems_list:
   highlighted_poems_stripped.append(element.strip())
 
- #
+ # Create a variable that splits the elements into each individual detail
 
-highlighted_poems_details =
+highlighted_poems_details = []
+
+for element in highlighted_poems_stripped:
+  highlighted_poems_details.append(element.split(":"))
+
+# Create a function that adds each respective element into it's respective variable
+index = 0
+titles = []
+poets = []
+dates = []
+
+for elements in highlighted_poems_details:
+  titles.append(elements[0])
+  poets.append(elements[1])
+  dates.append(elements[2])
+
+# Write a loop that formats the three variables in to a string
+
+message = "The poem {} was published by {} in {}"
+
+
+for elements in titles:
+  print(message.format(titles[index], poets[index], dates[index]))
+  index += 1
+
 
 # Printing commands
 print(highlighted_poems)
@@ -22,3 +46,13 @@ print("")
 print(highlighted_poems_list)
 print("")
 print(highlighted_poems_stripped)
+print("")
+print(highlighted_poems_details)
+print("")
+print(titles)
+print("")
+print(poets)
+print("")
+print(dates)
+print("")
+print(message.format(titles[0], poets[0], dates[0]))
